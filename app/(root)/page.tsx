439646dde0cff061c40d27fd21b023c528a21bc6
Siri503 <sirikanthiachanta@gmail.com>
2024-04-17 15:45:55 +0530

    header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/ui/button";
+
+
+export default function Home() {
+  return (
+   <main>
+    {/* <Button variant="destructive" className="px-10">Delete</Button> */}
+   </main>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+    publicRoutes:[
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing',
+        '/assets/images/logo.svg',
+        '/favicon.ico', 
+    ],
+    ignoredRoutes:[
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing',  
+    ]
+});
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
